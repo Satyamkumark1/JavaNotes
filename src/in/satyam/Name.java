9c9199f465999bb30f6743dcfb6922838ff918e6
Satyam <satyamnagar26@gmial.com>
2025-06-10 23:15:59 +0530

    firstcommit

---
+++ src/in/satyam/Name.java
@@ -0,0 +1,16 @@
+package in.satyam;
+
+public class Name {
+    private  String firstName = "xxoxo";
+    private String lastName;
+
+
+    public String setFirstName(String firstName) {
+        this.firstName = firstName;
+        return firstName;
+    }public String setLastName(String lastName) {
+        this.lastName = lastName;
+        return lastName;
+    }
+
+}
